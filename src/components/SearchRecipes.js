49f9236ff1786dce47c82363dea5fe928169e644
Mahjabeen Sultana <mahjabeen.sultana2012@gmail.com>
2019-11-10 12:28:28 -0600

    create form

--- src/components/SearchRecipes.js
+++ src/components/SearchRecipes.js
@@ -1,8 +1,21 @@
 import React, {Component} from 'react';
+import {Form, FormGroup, FormControl, ControlLabel, Button} from 'react-bootstrap'
 
 class SearchRecipes extend Component{
   render (){
-    return()
+    return(
+      <Form>
+        <FormGroup>
+          <ControlLabel>Ingredients</ControlLabel>
+          <FormControl type= "text" placeholder="mashroom" />>
+        </FormGroup>
+        {' '}
+        <FormGroup>
+        <ControlLabel>Dish</ControlLabel>
+        <FormControl type= "text" placeholder="pizza" />
+        </FormGroup>
+      </Form>
+    )
   }
 }
 
