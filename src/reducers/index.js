3645dd81134658647e1a56e82f1249387dd10b6a
Mahjabeen Sultana <mahjabeen.sultana2012@gmail.com>
2019-11-11 10:53:13 -0600

    found data

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -4,7 +4,7 @@ import { SET_RECIPES } from '../actions';
 function recipes(state = [], action) {
   switch (action.type) {
     case SET_RECIPES:
-      return action.type;
+      return action.items;
     default:
       return state;
   }
