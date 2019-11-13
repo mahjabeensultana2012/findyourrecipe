628443ee86265d1f82472e63ae81b7d1c8f0c3ac
Mahjabeen Sultana <mahjabeen.sultana2012@gmail.com>
2019-11-13 11:14:12 -0600

    add style for favorite

--- src/components/RecipeItem.js
+++ src/components/RecipeItem.js
@@ -8,7 +8,9 @@ class RecipeItem extends Component {
     let { recipe } = this.props;
     return (
       <div className="recipe-item">
-        <div onClick={() => this.props.favoriteRecipe(recipe)}>&#9734;</div>
+        <div className="star" onClick={() => this.props.favoriteRecipe(recipe)}>
+          &#9734;
+        </div>
         <div className="recipe-text">
           <a href={recipe.href}>
             <h4>{recipe.title}</h4>
