21ee5b7b29ea34a7a7a420cbb6fd3eb9ecc9db38
Mahjabeen Sultana <mahjabeen.sultana2012@gmail.com>
2019-11-14 17:39:51 -0600

    add favorite list

--- src/components/RecipeItem.js
+++ src/components/RecipeItem.js
@@ -3,6 +3,19 @@ import { connect } from 'react-redux';
 import { favoriteRecipe } from '../actions';
 
 class RecipeItem extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      favorited: false,
+    };
+  }
+
+  favorite(recipe) {
+    this.props.favoriteRecipe(recipe);
+    this.setState({ favorited: true });
+  }
+
   render() {
     console.log('AAA:', this.props);
     let { recipe } = this.props;
