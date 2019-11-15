f048654d325f80e230a2aa3bbc27de0bb81b06c5
Mahjabeen Sultana <mahjabeen.sultana2012@gmail.com>
2019-11-14 18:25:38 -0600

    add FavoriteRecipeList Component

---
+++ src/components/FavoriteRecipeList.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+//import { favoriteRecipe} from '../actions';
+import RecipeItem from './RecipeItem';
+
+class FavoriteRecipeList extends Component {
+  render() {
+    return (
+      <div>
+        <h4>Favorite List</h4>
+        {this.props.favoriteRecipes.map((recipe, index) => {
+          return <RecipeItem key={index} recipe={recipe} />;
+        })}
+      </div>
+    );
+  }
+}
+
+function mapStateToProps(state) {
+  return {
+    favoriteRecipes: state.favoriteRecipes,
+  };
+}
+
+export default connect(
+  mapStateToProps,
+  null
+)(FavoriteRecipeList);
