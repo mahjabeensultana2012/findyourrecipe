1ec7099d41faf9a4610542dd0b35fc634407b19e
Mahjabeen Sultana <mahjabeen.sultana2012@gmail.com>
2019-11-14 19:38:32 -0600

    add routing for home and favoritelist

--- src/components/FavoriteRecipeList.js
+++ src/components/FavoriteRecipeList.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-//import { favoriteRecipe} from '../actions';
+import { Link } from 'react-router-dom';
 import RecipeItem from './RecipeItem';
 
 class FavoriteRecipeList extends Component {
   render() {
     return (
       <div>
+        <h4>
+          <Link to="/">Home</Link>
+        </h4>
         <h4>Favorite List</h4>
         {this.props.favoriteRecipes.map((recipe, index) => {
           return (
--- src/components/RecipeList.js
+++ src/components/RecipeList.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import RecipeItem from './RecipeItem';
 
 class RecipeList extends Component {
@@ -7,6 +8,9 @@ class RecipeList extends Component {
     //console.log('this.props', this.props);
     return (
       <div>
+        <h4>
+          <Link to="/favorites">Favorite List</Link>
+        </h4>
         {this.props.recipes.map((recipe, index) => {
           return <RecipeItem key={index} recipe={recipe} recipeButton={true} />;
         })}
